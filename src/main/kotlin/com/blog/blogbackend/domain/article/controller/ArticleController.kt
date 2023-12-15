7df610a84d0f38754db0b234d4dec5a31b26e2d9
silveriron <kichul7493@gmail.com>
2023-12-15 23:51:43 +0900

    /api/articles 생성

---
+++ src/main/kotlin/com/blog/blogbackend/domain/article/controller/ArticleController.kt
@@ -0,0 +1,20 @@
+package com.blog.blogbackend.domain.article.controller
+
+import com.blog.blogbackend.domain.article.service.ArticleService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api")
+@RestController
+class ArticleController(
+  val articleService: ArticleService
+) {
+
+  @GetMapping("/articles")
+  fun getArticles(@RequestParam offset: Int, @RequestParam limit: Int) = articleService.getArticles(offset, limit)
+
+
+
+}
